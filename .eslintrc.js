0609cade5002a4e74a7fda3af200e16f4b9f6645
Alex <arevaloalex9@gmail.com>
2021-04-04 19:36:48 -0500

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: ['plugin:react/recommended', 'standard', 'prettier'],
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 12,
+		sourceType: 'module',
+	},
+	plugins: ['react', 'prettier'],
+	rules: {
+		'react/react-in-jsx-scope': 0,
+		'prettier/prettier': [
+			'error',
+			{
+				printWidth: 80,
+				trailingComma: 'es5',
+				semi: false,
+				jsxSingleQuote: true,
+				singleQuote: true,
+				useTabs: true,
+				endOfLine: 'auto',
+			},
+		],
+	},
+	settings: {
+		react: {
+			version: 'detected',
+		},
+	},
+}
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from 'antd'
+import './App.css'
+
+const App = () => {
+	return (
+		<div className='App'>
+			<Button type='primary'>Buttodnasd</Button>
+		</div>
+	)
+}
+
+export default App
